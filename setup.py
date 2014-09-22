a0b5573961e0b39bcfc3587143c0fd4fb21f9a14
Kieran Spear <kispear@gmail.com>
2014-09-22 12:17:20 +1000

    Task for monitoring rabbit queues
    Will optionally kill dead API processes when it finds a stuck queue
    to workaround the stuck reply queue issue.
    
    Change-Id: Ice8017dee7f1f0b4cabf4965ffdcc54c3eb2ff60

--- setup.py
+++ setup.py
@@ -19,6 +19,7 @@ requirements = [
     'python-novaclient',
     'MySQL-python',
     'prettytable',
+    'pyrabbit',
 ]
 
 test_requirements = [
@@ -69,6 +70,7 @@ setup(
       packaging = hivemind_contrib.packaging
       pbuilder = hivemind_contrib.pbuilder
       puppetdb = hivemind_contrib.puppetdb.tasks
+      rabbit = hivemind_contrib.rabbit
       rcshibboleth = hivemind_contrib.rcshibboleth
       repo = hivemind_contrib.repo
       swift = hivemind_contrib.swift
