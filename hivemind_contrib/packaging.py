a56d26bb7a626abd59f898a1d9aa676affd58c4f
Russell Sim <russell.sim@gmail.com>
2014-09-29 15:02:01 +1000

    Support repositories that have just debian branch
    Change-Id: I9051fe220e23e630fcf0a3e1c96ad1aba198ea87

--- hivemind_contrib/packaging.py
+++ hivemind_contrib/packaging.py
@@ -170,6 +170,8 @@ def buildpackage(release=None, upload=True):
         deb_branch = debian_branch(version)
         if not git.branch_exists(deb_branch):
             deb_branch = "debian/{0}".format(release)
+        if not git.branch_exists(deb_branch):
+            deb_branch = 'debian'
         assert git.branch_exists(deb_branch), \
             "Debian branch %s doesn't exist" % deb_branch
 
