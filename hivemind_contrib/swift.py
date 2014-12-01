b92e225faf4cf461265d6cfef8d7255235d1e4ef
Russell Sim <russell.sim@gmail.com>
2014-12-01 16:57:05 +1100

    Added swift quota command
    Change-Id: I3e1f7d3b2e72267af0795fe4bb5d3a1ce41b2fee

--- hivemind_contrib/swift.py
+++ hivemind_contrib/swift.py
@@ -148,3 +148,38 @@ def backup_ring():
     if 'swift-proxy' in services:
         run("cp /etc/swift/***.builder /etc/swift/backup_upgrade/",
             warn_only=True, quiet=True)
+
+
+def size_to_bytes(num):
+    units = ['B', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB']
+    if num[-2:].isalpha():
+        num, unit = (num[:-2], num[-2:])
+    elif num[-1:].isalpha():
+        num, unit = (num[:-1], num[-1])
+    else:
+        raise ValueError("No units found in number '%s'" % num)
+    try:
+        power = units.index(unit)
+    except:
+        raise ValueError("Unit %s is not one of %s" % (unit, units))
+    return int(num) * pow(1042, power)
+
+
+@task
+def set_quota(tenant_id, quota):
+    """Set the swift quota for a tenant.
+
+    :param str quota: A number in bytes or None for unlimited.
+    """
+    if quota.lower() == "none":
+        quota = ''
+    else:
+        quota = size_to_bytes(quota)
+    sc = client()
+    url, token = sc.get_auth()
+    base_url = url.split('_')[0] + '_'
+    tenant_url = base_url + tenant_id
+
+    swift_client.post_account(url=tenant_url,
+                              token=token,
+                              headers={'X-Account-Meta-Quota-Bytes': quota})
---
+++ tests/test_swift.py
@@ -0,0 +1,10 @@
+from hivemind_contrib import swift
+
+
+def test_size_to_bytes():
+    assert swift.size_to_bytes("1234567B") == 1234567
+    assert swift.size_to_bytes("123456MB") == 134044080384
+    assert swift.size_to_bytes("12345GB") == 13966714356360
+    assert swift.size_to_bytes("1234TB") == 1454742194200864
+    assert swift.size_to_bytes("123PB") == 151092778008061536
+    assert swift.size_to_bytes("12ZB") == 16004985270355602494976L
