5217d40c141f058aa5faf2e8436070e7df932d95
Andy Botting <andy@andybotting.com>
2016-07-01 10:05:11 +1000

    Ignore missing FreshDesk package if not needed
    Change-Id: I049e8672b0f34fe5c3e431ba03f6e03d7010a146

--- hivemind_contrib/security.py
+++ hivemind_contrib/security.py
@@ -9,7 +9,11 @@ from hivemind_contrib import nova
 from hivemind_contrib.show import generate_instance_info
 from hivemind_contrib.show import generate_instance_sg_rules_info
 
-from freshdesk.v2.api import API
+try:
+    from freshdesk.v2.api import API
+except:
+    # OK missing for testing
+    API = None
 
 
 @decorators.configurable('freshdesk')
@@ -38,6 +42,8 @@ def get_freshdesk_config(api_key=None,
 
 def get_freshdesk_client():
     domain, api_key = get_freshdesk_config()
+    if not API:
+        error("You will need to install python-freshdesk to use this function")
     return API(domain, api_key)
 
 
