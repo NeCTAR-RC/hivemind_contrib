dc1e396fa6990d278d5322677d506c50fecca837
Sam Morrison <sorrison@gmail.com>
2017-02-17 14:27:29 +1100

    Keystone V3 changes
    Change-Id: I7b29d5e299ee02210e1173a17d128bc607987c00

--- hivemind_contrib/neutron.py
+++ hivemind_contrib/neutron.py
@@ -1,19 +1,11 @@
-import os
+
+from neutronclient.neutron import client
 
 from hivemind.decorators import configurable
-from neutronclient.neutron import client
+from hivemind_contrib import keystone as hv_keystone
 
 
 @configurable('nectar.openstack.client')
-def get_neutron_client(version='2.0', url=None, username=None,
-                       password=None, tenant=None, region=None):
-    url = os.environ.get('OS_AUTH_URL', url)
-    username = os.environ.get('OS_USERNAME', username)
-    password = os.environ.get('OS_PASSWORD', password)
-    tenant = os.environ.get('OS_TENANT_NAME', tenant)
-    region = os.environ.get('OS_REGION_NAME', region)
-    assert url and username and password and tenant
-
-    return client.Client(version, auth_url=url, username=username,
-                         password=password, tenant_name=tenant,
-                         region_name=region, no_cache=True)
+def get_neutron_client(version='2.0'):
+    sess = hv_keystone.get_session()
+    return client.Client(version, session=sess)
