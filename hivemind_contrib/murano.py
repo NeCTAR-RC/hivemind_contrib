2bc64f5df0c059a47b843f22f75f9751d8f69906
Andy Botting <andy@andybotting.com>
2017-03-09 14:49:49 +1100

    Fix murano tool
    With the recent keystone changes, this tool had stopped working.
    I've moved to instantiating the keystone client though
    os-client-config and renamed tenant references to project.
    
    Change-Id: I9c2f6335624d33f979c9a70f3af1ac06620c8df5

--- hivemind_contrib/murano.py
+++ hivemind_contrib/murano.py
@@ -2,17 +2,15 @@ from fabric.api import task
 
 import os_client_config
 
-from hivemind_contrib import keystone
 
-
-def murano_client(url=None, username=None, password=None, tenant=None):
+def murano_client(url=None, username=None, password=None, project=None):
     return os_client_config.make_client('application-catalog', auth_url=url,
                                         username=username, password=password,
-                                        project_name=tenant)
+                                        project_name=project)
 
 
-def is_in_project(tenant, user):
-    return any([u == user for u in tenant.list_users()])
+def is_in_project(project, user):
+    return any([u == user for u in project.list_users()])
 
 
 @task
@@ -25,29 +23,29 @@ def private_package(package_id, dry_run=True):
     m = os_client_config.make_client('application-catalog')
 
     package = m.packages.get(package_id)
-    tenant_id = package.owner_id
+    project_id = package.owner_id
 
-    ks_client = keystone.client('3')
-    tenant = keystone.get_tenant(ks_client, tenant_id)
-    tenant_admin_role = ks_client.roles.find(name='Admin')
-    admin_user = keystone.get_user(ks_client, ks_client.user_id)
+    ks_client = os_client_config.make_client('identity')
+    project = ks_client.tenants.get(project_id)
+    project_admin_role = ks_client.roles.find(name='Admin')
+    admin_user = ks_client.users.get(ks_client.session.get_user_id())
 
-    if not is_in_project(tenant, admin_user):
-        print("Adding user to project %s (%s)" % (tenant.name, tenant.id))
-        tenant.add_user(admin_user, tenant_admin_role)
+    if not is_in_project(project, admin_user):
+        print("Adding user to project %s (%s)" % (project.name, project.id))
+        project.add_user(admin_user, project_admin_role)
 
     if package.is_public:
         if dry_run:
             print("Would remove public flag for package %s" % package_id)
         else:
             print("Removing public flag for package %s for %s" %
-                  (package_id, tenant_id))
+                  (package_id, project_id))
             m = os_client_config.make_client('application-catalog',
-                                             tenant_name=tenant.name)
+                                             project_name=project.name)
             m.packages.toggle_public(package_id)
     else:
         print("Package is not public")
 
-    if is_in_project(tenant, admin_user):
-        print("Removing user from project %s (%s)" % (tenant.name, tenant.id))
-        tenant.remove_user(admin_user, tenant_admin_role)
+    if is_in_project(project, admin_user):
+        print("Removing user from project %s (%s)" % (project.name, project.id))
+        project.remove_user(admin_user, project_admin_role)
