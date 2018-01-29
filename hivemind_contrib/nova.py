02d5f301b7f721665002fb139e0854d6dcf2746b
Rocky <shi.yan@unimelb.edu.au>
2018-01-30 00:27:49 +1100

    Make list_instances to better search instances
    Extra: Fix the issue when count of instances equal to 0
    
    Change-Id: I402b9a053f81b5748895acd173f29d48504128b8

--- hivemind_contrib/nova.py
+++ hivemind_contrib/nova.py
@@ -1,8 +1,9 @@
+import collections
+import os_client_config
+import re
 import sys
 import time
 import urlparse
-
-import os_client_config
 
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
@@ -18,7 +19,7 @@ from sqlalchemy import Table
 from hivemind import decorators
 from hivemind.operations import run
 from hivemind.util import current_host
-
+from hivemind_contrib import keystone
 from hivemind_contrib.swift import client as swift_client
 
 DEFAULT_AZ = 'melbourne-qh2'
@@ -118,30 +119,151 @@ def server_address(client, id):
                 return address['addr']
 
 
-def all_servers(client, limit=None, host=None, status=None, image=None):
-    servers = []
+def all_servers(client, zone=None, host=None, status=None, ip=None,
+                image=None, project=None, user=None, limit=None):
     marker = None
-    opts = {"all_tenants": True}
-    if host:
-        opts['host'] = host
+    opts = {}
+    opts["all_tenants"] = True
     if status:
         opts['status'] = status
     if limit:
         opts['limit'] = limit
     if image:
         opts['image'] = image
-    while True:
-        if marker:
-            opts["marker"] = marker
-        result = client.servers.list(search_opts=opts)
-        if not result:
-            break
-        servers.extend(result)
-        # Quit if we have got enough servers.
-        if limit and len(servers) >= int(limit):
-            break
-        marker = servers[-1].id
-    return servers
+    if project:
+        opts['tenant_id'] = project
+    if user:
+        opts['user_id'] = keystone.get_user(keystone.client(), user).id
+
+    hosts_list = parse_nodes(host) if host else None
+    az_list = parse_nodes(zone) if zone else None
+    ip_list = parse_nodes(ip) if ip else None
+
+    if host:
+        for host in hosts_list:
+            opts['host'] = host
+            instances = client.servers.list(search_opts=opts)
+            if not instances:
+                break
+            instances = filter(lambda x: match_availability_zone(x, az_list),
+                               instances)
+            instances = filter(lambda x: match_ip_address(x, ip_list),
+                               instances)
+            return instances
+    else:
+        while True:
+            if marker:
+                opts['marker'] = marker
+            instances = client.servers.list(search_opts=opts)
+            if not instances:
+                return
+            instances = filter(lambda x: match_availability_zone(x, az_list),
+                               instances)
+            instances = filter(lambda x: match_ip_address(x, ip_list),
+                               instances)
+            if not instances:
+                break
+            marker = instances[-1].id
+            return instances
+
+
+def match_availability_zone(server, az=None):
+    if az:
+        if getattr(server, "OS-EXT-AZ:availability_zone") not in az:
+            return False
+    return True
+
+
+def match_ip_address(server, ips):
+    if not ips:
+        return True
+    for ip in ips:
+        if ip == server.accessIPv4 or ip == server.accessIPv6:
+            return True
+        if any(map(lambda a: any(map(lambda aa: ip in aa['addr'], a)),
+                        server.addresses.values())):
+            return True
+    return False
+
+
+def extract_server_info(server):
+    server_info = collections.defaultdict(dict)
+    try:
+        server_info['id'] = server.id
+        server_info['name'] = server.name
+        server_info['status'] = server.status
+        server_info['image'] = server.image['id']
+        server_info['flavor'] = server.flavor['id']
+        server_info['host'] = getattr(server, "OS-EXT-SRV-ATTR:host")
+        server_info['zone'] = getattr(server, "OS-EXT-AZ:availability_zone")
+
+        # handle some tier2 services which using "global" service user/project
+        if server.metadata and 'user_id' in server.metadata.keys()\
+            and 'project_id' in server.metadata.keys():
+            server_info['user'] = server.metadata['user_id']
+            server_info['project'] = server.metadata['project_id']
+        else:
+            server_info['user'] = server.user_id
+            server_info['project'] = server.tenant_id
+
+        server_info['project_name'] =\
+                keystone.get_project(keystone.client(),
+                                     server_info['project']).name
+
+        server_info['accessIPv4'] = extract_ip(server)
+        user = keystone.get_user(keystone.client(), server_info['user'])
+
+        # handle instaces created by jenkins/tempest etc.
+        if user.email:
+            server_info['email'], server_info['fullname']\
+                    = user.email, user.full_name
+        else:
+            server_info['email'], server_info['fullname']\
+                    = user.name, None
+    except KeyError as e:
+        raise type(e)(e.message + ' missing in context: %s' % server.to_dict())
+
+    return server_info
+
+
+def extract_ip(server):
+    addresses = set()
+    if server.accessIPv4:
+        addresses.add(server.accessIPv4)
+    elif server.accessIPv6:
+        addresses.add(server.accessIPv6)
+
+    for address in server.addresses.values():
+        for addr in address:
+            if addr['addr']:
+                addresses.add(addr['addr'])
+    return list(addresses)
+
+
+def parse_dash(range_str):
+    """Unpack the dash syntax into a set of number"""
+    hosts = range_str.split("-")
+    return range(int(hosts[0]), int(hosts[1]) + 1) if len(hosts) > 1 \
+                 else [range_str]
+
+
+def parse_nodes(nodes):
+    """Parse list syntax (eg. qh2-rcc[01-10,13])"""
+
+    # Parse qh2-rcc5,qh2-rcc6,qh2-rcc7 syntax
+    nodes = re.split(r",\s*(?![^\[\]]*\])", nodes)
+
+    if len(nodes) > 1:
+        nodes = set(host for hosts in nodes for host in parse_nodes(hosts))
+    else:
+        # Parse qh2-rcc[112-114,115] syntax
+        match = re.search(r"(.*?)\[(.*?)\](.*)", nodes[0])
+        if match:
+            host_ranges = [host for hosts in parse_nodes(match.group(2))
+                           for host in parse_dash(hosts)]
+            nodes = set("%s%s%s" % (match.group(1), host, match.group(3))
+                        for host in host_ranges)
+    return nodes
 
 
 def combine_files(file_contents):
@@ -275,3 +397,23 @@ def list_host_aggregates(availability_zone, hostname=[]):
         table.add_row(row)
 
     print(table)
+
+
+@task
+@decorators.verbose
+def list_instances(zone=None, nodes=None, project=None, user=None, status=None,
+                   ip=None, image=None, limit=None):
+    novaclient = client()
+    result = all_servers(novaclient, zone=None, nodes=None, status=None,
+                         ip=None, image=None, project=None, user=None,
+                         limit=None)
+    result = map(extract_server_info, result)
+    header = None
+    for inst in result:
+        if not header:
+            header = inst.keys()
+            table = PrettyTable(header)
+        table.add_row(inst.values())
+    print(table)
+    print("number of instances:", len(result))
+    return result
