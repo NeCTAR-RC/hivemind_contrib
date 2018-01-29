c190e456f270dfb0e8f90cea78c7bf7b5a016367
Rocky <shi.yan@unimelb.edu.au>
2018-01-29 23:42:48 +1100

    Add roles into keystone list_member
    Change-Id: I0509a858944d8c4242eb41542fcc24a642dc22b1

--- hivemind_contrib/keystone.py
+++ hivemind_contrib/keystone.py
@@ -179,43 +179,59 @@ def clear_user_metadata(user, key):
     set_user_metadata(user, key, None)
 
 
-def get_members(keystone, project):
+def get_members(keystone, project, roles=None):
     """get members of a project
 
     :return: {userid: { username, [projectname, ...] } for each user
     """
-    role_assignments = keystone.role_assignments.list(project=project.id,
-                                                      include_names=True)
+    if not roles:
+        role_assignments = keystone.role_assignments.list(project=project.id,
+                                                          include_names=True)
+    else:
+        role_assignments = []
+        for role in roles:
+            role_assignments.extend(keystone.role_assignments.list(
+                project=project.id, include_names=True, role=role))
+
     users = {}
     for ra in role_assignments:
-
         if ra.user['id'] in users:
             users[ra.user['id']]['roles'].append(ra.role['name'])
         else:
             users[ra.user['id']] = {}
             users[ra.user['id']]['name'] = ra.user['name']
             users[ra.user['id']]['roles'] = [ra.role['name']]
+            users[ra.user['id']]['project_name']\
+                    = ra.scope['project']['name']
     return(users)
 
 
-def print_members(keystone, project):
-    table = PrettyTable(["ID", "Username", "Roles"])
+def print_members(keystone, project, roles=None):
+    table = PrettyTable(["ID", "Username", "Roles", "Project_Name"])
 
-    users = get_members(keystone, project)
+    users = get_members(keystone, project, roles)
 
     for user_id, attrs in users.items():
-        table.add_row([user_id, attrs['name'], ", ".join(attrs['roles'])])
+        table.add_row([user_id, attrs['name'], ", ".join(attrs['roles']),
+                      attrs['project_name']])
     print("Members of %s (%s):" % (project.name, project.id))
     print(str(table))
+    return users
 
 
 @task
 @decorators.verbose
-def list_members(project):
-    """List members of a tenant"""
+def list_members(project, select_roles=None):
+    """List members of a tenant with sepecific roles
+    If the select_roles is not sepecified, all roles are allowed
+    """
     keystone = client()
     project = get_project(keystone, project)
-    print_members(keystone, project)
+    roles_id = None
+    if select_roles:
+        select_roles = select_roles.split(",")
+        roles_id = map(lambda r: keystone.roles.find(name=r).id, select_roles)
+    return print_members(keystone, project, roles_id)
 
 
 def has_role_in_project(project, user, role):
