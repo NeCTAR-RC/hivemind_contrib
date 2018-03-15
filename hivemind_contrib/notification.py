6a874b2b95fd5bfb72728b1058b5508a3ca45a8e
Nhat Ngo <phuquoc2001@gmail.com>
2018-03-15 15:40:59 +1100

    Fix announcement_mailout broke on Unicode template
    Change-Id: Ib410cfff00a1328583e95621cdcb652bc5d49614

--- hivemind_contrib/notification.py
+++ hivemind_contrib/notification.py
@@ -18,6 +18,8 @@ from prettytable import PrettyTable
 
 import collections
 import datetime
+
+import io
 import os
 import re
 import smtplib
@@ -249,7 +251,7 @@ def generate_notification_mails(subject, template, data, work_dir,
                                                    end_time, timezone, zone,
                                                    affected, nodes)
             filename = user + "@" + proj
-            with open(os.path.join(work_dir, filename), 'wb') as mail:
+            with io.open(os.path.join(work_dir, filename), 'w') as mail:
                 recipient += 1
                 mail.write(msg)
 
@@ -371,9 +373,8 @@ def mailout(work_dir, data, subject, config, cc=None):
     print("Starting emails sending.....")
     for mail in mails:
         if is_email_address(mail[0]):
-            with open(os.path.join(work_dir, '@'.join(mail)), 'rb') as f:
+            with io.open(os.path.join(work_dir, '@'.join(mail)), 'rb') as f:
                 text = f.read()
-
             cc_list = []
             if cc:
                 for c in cc:
