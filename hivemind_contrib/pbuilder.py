56232f02e2201e8bf445ef17bdd987c128749409
Sam Morrison <sorrison@gmail.com>
2018-07-23 11:07:36 +1000

    Support packaging based on just ubuntu version.
    Currently packaging is based around only openstack version. This
    is still the default but you can now pass in a ubuntu version as the
    openstack release to build a non openstack version.
    
    You can now also override the ubuntu release for the openstack release.
    This will allow you to build eg. xenial-queens and bionic-queens packages.
    
    Change-Id: If203e0dcf431746bb77ec54c77ab2a41b7076a15

--- hivemind_contrib/pbuilder.py
+++ hivemind_contrib/pbuilder.py
@@ -19,15 +19,29 @@ ARCH = "amd64"
 STABLE_RELEASE = "mitaka"
 OPENSTACK_RELEASES = ['queens', 'pike', 'ocata', 'newton', 'mitaka',
                       'liberty', 'kilo']
+UBUNTU_RELEASES = ['trusty', 'xenial', 'bionic']
+DEFAULT_UBUNTU = 'xenial'
 NECTAR_REPO = 'http://download.rc.nectar.org.au/nectar-ubuntu/'
 CLOUD_ARCHIVE = 'http://download.rc.nectar.org.au/ubuntu-cloud/ubuntu/'
 UBUNTU_MIRROR = 'http://download.rc.nectar.org.au/ubuntu-archive/ubuntu/'
 
 
 def dist_from_release(release):
+    if release in UBUNTU_RELEASES:
+        return release
     if release in ['liberty', 'kilo']:
         return 'trusty'
-    return 'xenial'
+    return DEFAULT_UBUNTU
+
+
+def get_build_env(os_release, ubuntu_release=None):
+    if os_release in UBUNTU_RELEASES:
+        return os_release
+    if not ubuntu_release:
+        if os_release in ['liberty', 'kilo']:
+            ubuntu_release = 'trusty'
+        ubuntu_release = DEFAULT_UBUNTU
+    return "%s-%s" % (ubuntu_release, os_release)
 
 
 def apt_key_recv_key(key_id, keyring):
@@ -41,6 +55,7 @@ def build_trusted():
     local("touch {0}".format(db))
     apt_key_recv_key("5EDB1B62EC4926EA", db)
     apt_key_recv_key("40976EAF437D05B5", db)
+    apt_key_recv_key("3B4FE6ACC0B21F32", db)
     with tempfile.NamedTemporaryFile() as tmp_gpg:
         response = requests.get(NECTAR_REPO + "nectar-custom.gpg")
         tmp_gpg.write(response.content)
@@ -58,14 +73,27 @@ def build_trusted():
 
 
 mirrors = {
-    'kilo': [
+    'xenial': [
+        "deb " + CLOUD_ARCHIVE + " xenial-updates/queens main",
+        "deb " + NECTAR_REPO + " xenial main",
+        "deb " + NECTAR_REPO + " xenial-queens main",
+        "deb " + NECTAR_REPO + " xenial-queens-testing main",
+        "deb " + NECTAR_REPO + " xenial-testing main",
+        "deb " + UBUNTU_MIRROR + " xenial-updates main universe"],
+    'bionic': [
+        "deb " + NECTAR_REPO + " bionic main",
+        "deb " + NECTAR_REPO + " bionic-queens main",
+        "deb " + NECTAR_REPO + " bionic-queens-testing main",
+        "deb " + NECTAR_REPO + " bionic-testing main",
+        "deb " + UBUNTU_MIRROR + " bionic-updates main universe"],
+    'trusty-kilo': [
         "deb " + CLOUD_ARCHIVE + " trusty-updates/kilo main",
         "deb " + NECTAR_REPO + " trusty main",
         "deb " + NECTAR_REPO + " trusty-kilo main",
         "deb " + NECTAR_REPO + " trusty-kilo-testing main",
         "deb " + NECTAR_REPO + " trusty-testing main",
         "deb " + UBUNTU_MIRROR + " trusty-updates main universe"],
-    'liberty': [
+    'trusty-liberty': [
         "deb " + CLOUD_ARCHIVE + " trusty-updates/liberty main",
         "deb " + NECTAR_REPO + " trusty main",
         "deb " + NECTAR_REPO + " trusty-liberty main",
@@ -79,45 +107,52 @@ mirrors = {
         "deb " + NECTAR_REPO + " trusty-mitaka-testing main",
         "deb " + NECTAR_REPO + " trusty-testing main",
         "deb " + UBUNTU_MIRROR + " trusty-updates main universe"],
-    'mitaka': [
+    'xenial-mitaka': [
         "deb " + NECTAR_REPO + " xenial main",
         "deb " + NECTAR_REPO + " xenial-mitaka main",
         "deb " + NECTAR_REPO + " xenial-mitaka-testing main",
         "deb " + NECTAR_REPO + " xenial-testing main",
         "deb " + UBUNTU_MIRROR + " xenial-updates main universe"],
-    'newton': [
+    'xenial-newton': [
         "deb " + CLOUD_ARCHIVE + " xenial-updates/newton main",
         "deb " + NECTAR_REPO + " xenial main",
         "deb " + NECTAR_REPO + " xenial-newton main",
         "deb " + NECTAR_REPO + " xenial-newton-testing main",
         "deb " + NECTAR_REPO + " xenial-testing main",
         "deb " + UBUNTU_MIRROR + " xenial-updates main universe"],
-    'ocata': [
+    'xenial-ocata': [
         "deb " + CLOUD_ARCHIVE + " xenial-updates/ocata main",
         "deb " + NECTAR_REPO + " xenial main",
         "deb " + NECTAR_REPO + " xenial-ocata main",
         "deb " + NECTAR_REPO + " xenial-ocata-testing main",
         "deb " + NECTAR_REPO + " xenial-testing main",
         "deb " + UBUNTU_MIRROR + " xenial-updates main universe"],
-    'pike': [
+    'xenial-pike': [
         "deb " + CLOUD_ARCHIVE + " xenial-updates/pike main",
         "deb " + NECTAR_REPO + " xenial main",
         "deb " + NECTAR_REPO + " xenial-pike main",
         "deb " + NECTAR_REPO + " xenial-pike-testing main",
         "deb " + NECTAR_REPO + " xenial-testing main",
         "deb " + UBUNTU_MIRROR + " xenial-updates main universe"],
-    'queens': [
+    'xenial-queens': [
         "deb " + CLOUD_ARCHIVE + " xenial-updates/queens main",
         "deb " + NECTAR_REPO + " xenial main",
         "deb " + NECTAR_REPO + " xenial-queens main",
         "deb " + NECTAR_REPO + " xenial-queens-testing main",
         "deb " + NECTAR_REPO + " xenial-testing main",
         "deb " + UBUNTU_MIRROR + " xenial-updates main universe"],
+    'bionic-queens': [
+        "deb " + NECTAR_REPO + " bionic main",
+        "deb " + NECTAR_REPO + " bionic-queens main",
+        "deb " + NECTAR_REPO + " bionic-queens-testing main",
+        "deb " + NECTAR_REPO + " bionic-testing main",
+        "deb " + UBUNTU_MIRROR + " bionic-updates main universe"],
 }
 
 ubuntu_mirrors = {
     'trusty': 'http://download.rc.nectar.org.au/ubuntu-archive/ubuntu/',
     'xenial': 'http://download.rc.nectar.org.au/ubuntu-archive/ubuntu/',
+    'bionic': 'http://download.rc.nectar.org.au/ubuntu-archive/ubuntu/',
 }
 
 
@@ -130,9 +165,8 @@ def package_export_dir():
         return os.path.abspath(config.get('buildpackage', 'export-dir'))
 
 
-def pbuilder_env(os_release, name=None):
-    dist = dist_from_release(os_release)
-    dist_release = '{0}-{1}'.format(dist, os_release)
+def pbuilder_env(os_release, name=None, ubuntu_release=None):
+    dist_release = get_build_env(os_release, ubuntu_release)
     if name:
         dist_release = '{0}-{1}'.format(dist_release, name)
     output_dir = os.path.join(package_export_dir(), dist_release)
@@ -152,9 +186,9 @@ def create(os_release=None, extra_mirror=None, name=None):
     """Create an environment for building packages."""
     if os_release is None:
         os_release = get_os_release_from_current_branch()
-    dist = dist_from_release(os_release)
-    path = '/var/cache/pbuilder/base-{dist}-{os_release}'.format(
-        dist=dist, os_release=os_release)
+    dist_release = get_build_env(os_release)
+    path = '/var/cache/pbuilder/base-{dist_release}'.format(
+        dist_release=dist_release)
     if name:
         path = '{path}-{name}'.format(path=path, name=name)
     path = '{path}-{arch}.cow'.format(path=path, arch=ARCH)
@@ -165,14 +199,15 @@ def create(os_release=None, extra_mirror=None, name=None):
     build_trusted()
     keyring = expanduser("~/.trusted.gpg")
 
-    mirror = ubuntu_mirrors[dist]
-    other_mirrors = mirrors[os_release]
+    ubuntu_version = dist_release.split('-')[0]
+    mirror = ubuntu_mirrors[ubuntu_version]
+    other_mirrors = mirrors[dist_release]
     components = "main universe"
 
     if extra_mirror:
         other_mirrors.append(extra_mirror)
 
-    with shell_env(ARCH=ARCH, DIST=dist):
+    with shell_env(ARCH=ARCH, DIST=ubuntu_version):
         local('git-pbuilder create --basepath {basepath}'
               ' --mirror {mirror}'
               ' --components "{components}"'
@@ -189,19 +224,19 @@ def create(os_release=None, extra_mirror=None, name=None):
 
 @task
 @verbose
-def shell(os_release=None, name=None):
+def shell(os_release=None, name=None, ubuntu_release=None):
     """Open a shell in the packaging environment."""
     if os_release is None:
         os_release = get_os_release_from_current_branch()
-    with pbuilder_env(os_release, name):
+    with pbuilder_env(os_release, name, ubuntu_release):
         local("git-pbuilder login")
 
 
 @task
 @verbose
-def update(os_release=None, name=None):
+def update(os_release=None, name=None, ubuntu_release=None):
     """Update the packaging environment."""
     if os_release is None:
         os_release = get_os_release_from_current_branch()
-    with pbuilder_env(os_release, name):
+    with pbuilder_env(os_release, name, ubuntu_release):
         local("git-pbuilder update")
