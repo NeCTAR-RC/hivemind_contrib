86744c5e7243657307742b80517ce3cbdc68eaeb
Paul van den Bergen <paul.vandenbergen@ardc.edu.au>
2019-05-21 13:31:46 +1000

    adding gitea configurator to hivemind
    Change-Id: If9075577f3105dda3e17443d8cf1cf5303a0e74f

--- hivemind_contrib/code.py
+++ hivemind_contrib/code.py
@@ -4,6 +4,7 @@ from fabric.api import task
 from hivemind.decorators import verbose
 from hivemind import util
 from hivemind_contrib import gerrit
+from hivemind_contrib import gitea
 
 import github
 from github.GithubException import UnknownObjectException
@@ -23,30 +24,44 @@ def get_github_token():
 
 @task
 @verbose
-def setup_project(name, org_name='NeCTAR-RC', fork_from=None,
-                  openstack_version=None, team_id=338031,
-                  stable_ref='openstack/stable/'):
+def setup_project(
+    name,
+    org_name='NeCTAR-RC',
+    fork_from=None,
+    openstack_version=None,
+    team_id=338031,
+    stable_ref='openstack/stable/'
+    ):
     """Create a new project.
 
-    For github integration you will need to have to following in your global
-    git.config:
+    org_name
+        NeCTAR-RC implies github
+        internal => use url to specify
+                    e.g. git.rc.nectar.org.au
+            Use the list_teams flag to get a list of team name IDs
+            then exit with no other action
 
+    For github integration you will need to have to following in your global
+
+    git.config:
     [github]
     user  = <github-username>
     token = <github-api-token>
 
     NOTE: you need to run this command inside the directory of the git
     cloned repo.
+
     """
 
-    github_user = get_github_username()
-    github_token = get_github_token()
-    full_name = org_name + '/' + name
-
     if org_name == 'internal':
-        print('Need to create repo in gitolite')
         fork_repo = None
+        gitea.getteamIDs(org_name)
+        gitea.makerepo(org_name, name)
+        print("Creating repo %s/%s" % (org_name, name))
     else:
+        github_user = get_github_username()
+        github_token = get_github_token()
+        full_name = org_name + '/' + name
         g = github.Github(github_user, github_token)
         org = g.get_organization(org_name)
 
@@ -79,19 +94,19 @@ def setup_project(name, org_name='NeCTAR-RC', fork_from=None,
     try:
         gerrit.create(full_name, parent=parent)
         print("Added gerrit project %s" % full_name)
-    except:  # noqa
+    except:   # noqa
         pass
 
     gerrit_user = gerrit.gitreview_username()
     try:
         local('git remote rm origin')
-    except:  # noqa
+    except:   # noqa
         pass
 
     if fork_repo:
         try:
             local('git remote add openstack %s' % fork_repo.clone_url)
-        except:  # noqa
+        except:   # noqa
             pass
 
     if openstack_version:
@@ -103,13 +118,13 @@ def setup_project(name, org_name='NeCTAR-RC', fork_from=None,
         try:
             local('git remote add origin git@git.melbourne.nectar.org.au:%s' %
                   full_name)
-        except:  # noqa
+        except:   # noqa
             pass
     else:
         try:
             local('git remote add nectar https://github.com/%s.git' %
                   full_name)
-        except:  # noqa
+        except:   # noqa
             pass
 
     local('git fetch --all')
