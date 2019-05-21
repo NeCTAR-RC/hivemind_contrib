e6516457a6540dd13a5e559631d64009076b1dbf
Paul van den Bergen <paul.vandenbergen@ardc.edu.au>
2019-05-21 13:31:46 +1000

    adding gitea configurator to hivemind
    Change-Id: If9075577f3105dda3e17443d8cf1cf5303a0e74f

--- hivemind_contrib/code.py
+++ hivemind_contrib/code.py
@@ -1,12 +1,14 @@
 from fabric.api import local
 from fabric.api import task
+import github
+from github.GithubException import UnknownObjectException
+import json
+import requests
 
 from hivemind.decorators import verbose
 from hivemind import util
 from hivemind_contrib import gerrit
-
-import github
-from github.GithubException import UnknownObjectException
+from hivemind_contrib import gitea
 
 
 def get_github_username():
@@ -21,12 +23,58 @@ def get_github_token():
     return token
 
 
+def setup_gitea(org_name, name):
+    try:
+        gitea.create_repo(org_name, name)
+    except requests.exceptions.HTTPError as e:
+        if e.response.status_code == 409:
+            print(json.loads(e.response.text)['message'])
+            pass
+        else:
+            raise e
+    gitea.add_teams_to_repo(org_name, name)
+
+
+def setup_github(org_name, name, team_id, fork_from):
+    fork_repo = None
+    github_user = get_github_username()
+    github_token = get_github_token()
+    g = github.Github(github_user, github_token)
+    org = g.get_organization(org_name)
+
+    if fork_from:
+        fork_repo = g.get_repo(fork_from)
+        try:
+            repo = org.get_repo(name)
+        except UnknownObjectException:
+            repo = None
+        if not repo:
+            repo = org.create_fork(fork_repo)
+            print("Created fork %s" % repo.name)
+    else:
+        fork_repo = None
+        try:
+            repo = org.get_repo(name)
+        except UnknownObjectException:
+            repo = org.create_repo(name)
+            print("Created repo %s" % repo.name)
+
+    team = org.get_team(team_id)
+    team.add_to_repos(repo)
+    print("Added %s team to repo" % team.name)
+    return fork_repo
+
+
 @task
 @verbose
 def setup_project(name, org_name='NeCTAR-RC', fork_from=None,
                   openstack_version=None, team_id=338031,
                   stable_ref='openstack/stable/'):
     """Create a new project.
+
+    org_name:
+        NeCTAR-RC implies github
+        internal implies git.rc.nectar.org.au
 
     For github integration you will need to have to following in your global
     git.config:
@@ -37,39 +85,19 @@ def setup_project(name, org_name='NeCTAR-RC', fork_from=None,
 
     NOTE: you need to run this command inside the directory of the git
     cloned repo.
+
     """
 
-    github_user = get_github_username()
-    github_token = get_github_token()
     full_name = org_name + '/' + name
 
     if org_name == 'internal':
-        print('Need to create repo in gitolite')
+        print("adding repo to Gitea")
+        setup_gitea(org_name, name)
         fork_repo = None
-    else:
-        g = github.Github(github_user, github_token)
-        org = g.get_organization(org_name)
 
-        if fork_from:
-            fork_repo = g.get_repo(fork_from)
-            try:
-                repo = org.get_repo(name)
-            except UnknownObjectException:
-                repo = None
-            if not repo:
-                repo = org.create_fork(fork_repo)
-                print("Created fork %s" % repo.name)
-        else:
-            fork_repo = None
-            try:
-                repo = org.get_repo(name)
-            except UnknownObjectException:
-                repo = org.create_repo(name)
-                print("Created repo %s" % repo.name)
-
-        team = org.get_team(team_id)
-        team.add_to_repos(repo)
-        print("Added %s team to repo" % team.name)
+    if org_name == 'NeCTAR-RC':
+        print("adding repo to GitHub")
+        fork_repo = setup_github(org_name, name, team_id, fork_from)
 
     if org_name == 'NeCTAR-RC':
         parent = 'Public-Projects'
@@ -101,7 +129,7 @@ def setup_project(name, org_name='NeCTAR-RC', fork_from=None,
 
     if org_name == 'internal':
         try:
-            local('git remote add origin git@git.melbourne.nectar.org.au:%s' %
+            local('git remote add origin git@git.rc.nectar.org.au:%s' %
                   full_name)
         except:  # noqa
             pass
