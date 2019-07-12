0057b575522ee213f540b1b98ed8eb23202787d1
Adrian Smith <aussieade@gmail.com>
2019-07-12 13:30:50 +1000

    clean up duplicate allocations in db
    Change-Id: I4d6d3fa752b15762697e8c836e73deee591d95a1

--- hivemind_contrib/placement.py
+++ hivemind_contrib/placement.py
@@ -2,8 +2,11 @@ from __future__ import print_function
 
 from fabric.api import task
 from hivemind import decorators
+from hivemind_contrib import nova
+from novaclient import exceptions as n_exc
 from prettytable import PrettyTable
 from sqlalchemy import create_engine
+from sqlalchemy import func
 from sqlalchemy import MetaData
 from sqlalchemy import Table
 
@@ -43,10 +46,10 @@ def print_table(data):
 
 
 @task
-def show_allocations(instance_uuid, connection=None, debug=False):
-    """display all allocations in nova_api db for instance
+def show_allocation(instance_id, connection=None, debug=False):
+    """display allocations in nova_api db for instance
 
-    :param str instance_uuid: instance uuid
+    :param str instance_id: instance uuid
     :param str connection: specify db connection string
     :param bool debug (False): show sql calls
     """
@@ -67,22 +70,22 @@ def show_allocations(instance_uuid, connection=None, debug=False):
              alloc_tb.join(
                  res_tb,
                  alloc_tb.c.resource_provider_id == res_tb.c.id)).where(
-                     alloc_tb.c.consumer_id == instance_uuid)
+                     alloc_tb.c.consumer_id == instance_id)
 
     results = db_conn.execute(query).fetchall()
 
     if not results:
-        print("no allocation records found for instance %s\n" % instance_uuid)
+        print("no allocation records found for instance %s\n" % instance_id)
     else:
         print_table(results)
 
 
 @task
-def clean_allocations(instance_uuid, hypervisor, connection=None,
+def clean_allocation(instance_id, hypervisor, connection=None,
           debug=False, dry_run=True):
     """clean up multiple allocations in nova_api db for instance
 
-    :param str instance_uuid: instance uuid
+    :param str instance_id: instance uuid
     :param str hypervisor: hypervisor running the instance
     :param str connection: specify db connection string
     :param bool debug (False): show sql calls
@@ -106,12 +109,12 @@ def clean_allocations(instance_uuid, hypervisor, connection=None,
                   res_tb,
                   alloc_tb.c.resource_provider_id == res_tb.c.id)).where(
                       and_(res_tb.c.name != hypervisor,
-                           alloc_tb.c.consumer_id == instance_uuid))
+                           alloc_tb.c.consumer_id == instance_id))
 
     results = db_conn.execute(query).fetchall()
 
     if not results:
-        print("multiple allocation records not found for %s\n" % instance_uuid)
+        print("multiple allocation records not found for %s\n" % instance_id)
         return
 
     ids = [r[0] for r in results]
@@ -130,4 +133,86 @@ def clean_allocations(instance_uuid, hypervisor, connection=None,
         stmt = alloc_tb.delete().where(alloc_tb.c.id.in_(ids))
         db_conn.execute(stmt)
         print("remaining allocations:")
-        show_allocations(instance_uuid)
+        show_allocation(instance_id)
+
+
+def _get_dup_allocation_instances(connection, debug=False):
+    """get list of instance uuids with duplicate allocations
+
+    :param str connection: specify db connection string
+    :param bool debug (False): show sql calls
+    """
+    connection = get_connection(connection)
+    engine = connect(connection, debug)
+    db_conn = engine.connect()
+
+    alloc_tb = Table('allocations', metadata,
+                     autoload=True, autoload_with=engine)
+
+    query = select([alloc_tb.c.consumer_id]).where(
+        alloc_tb.c.resource_class_id == 0).having(
+            func.count(alloc_tb.c.consumer_id) > 1).group_by(
+                alloc_tb.c.consumer_id)
+
+    results = db_conn.execute(query).fetchall()
+    return results
+
+
+@task
+def show_duplicate_allocations(connection=None):
+    dups = _get_dup_allocation_instances(connection, False)
+    if not dups:
+        print("no duplicate allocation records found")
+    else:
+        print_table(dups)
+
+
+@task
+def clean_duplicate_allocations(connection=None, debug=False, dry_run=True):
+    nc = nova.client()
+    dups = _get_dup_allocation_instances(connection, debug=debug)
+    if not dups:
+        print("no duplicate allocation records found\n")
+        exit(0)
+    else:
+        for i in (dups):
+            try:
+                server = nc.servers.get(i[0])
+            except n_exc.NotFound:
+                _del_all_allocations_for_instance(connection,
+                                                  i[0], debug, dry_run)
+                continue
+            except Exception:
+                print('error retrieving instance {} details'.format(i[0]))
+                exit(1)
+
+            hypervisor = getattr(server,
+                                 'OS-EXT-SRV-ATTR:hypervisor_hostname', None)
+            if hypervisor is not None:
+                _del_dup_allocations_for_instance(connection, server.id,
+                                                  hypervisor, dry_run)
+            else:
+                print('couldnt find hypervisor for instance {}'.format(i[0]))
+                exit(1)
+
+
+def _del_all_allocations_for_instance(connection, id, debug, dry_run):
+    if dry_run:
+        print('{} not found, would delete all allocations'.format(id))
+    else:
+        connection = get_connection(connection)
+        engine = connect(connection, debug)
+        db_conn = engine.connect()
+
+        alloc_tb = Table('allocations', metadata,
+                         autoload=True, autoload_with=engine)
+        stmt = alloc_tb.delete().where(alloc_tb.c.consumer_id == id)
+        db_conn.execute(stmt)
+
+
+def _del_dup_allocations_for_instance(connection, id, host, dry_run):
+    if dry_run:
+        print('would call clean_allocation({}, {}, {})'.format(
+            id, host, dry_run))
+    else:
+        clean_allocation(id, host, connection, dry_run)
