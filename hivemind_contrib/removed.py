de266eb19d7a52921f86cf53e5a4b0547d041495
Adrian Smith <aussieade@gmail.com>
2020-05-05 10:43:17 +1000

    remove functions replaced with nectar-osc
    Change-Id: I38cf700381cc5a300bd3141cf46cb3a76fa0650c

---
+++ hivemind_contrib/removed.py
@@ -0,0 +1,7 @@
+from __future__ import print_function
+
+
+def moved_to_osc():
+    print("""This function has been moved to the nectar-osc client
+See the below wiki article to get started
+https://wiki.rc.nectar.org.au/wiki/Openstack_CLI_Tool""")
